ef752dcad7c54ddac02a00e6c6707d1f0495b014
I-Bouros <ioana.bouros@dtc.ox.ac.uk>
2023-01-03 16:08:58 +0200

    setup test folder

---
+++ warwickmodel/tests/__init__.py
@@ -0,0 +1,12 @@
+#
+# Test module for warwickmodel.
+#
+# This file is part of WARWICKMODEL
+# (https://github.com/I-Bouros/warwick-covid-transmission.git) which is
+# released under the MIT license. See accompanying LICENSE for copyright
+# notice and full license details.
+#
+# To run all tests, use ``python -m unittest discover``.
+#
+# To run a particular test, use e.g.
+#  ``python -m unittest warwickmodel.tests.test_setup_matrices``.
